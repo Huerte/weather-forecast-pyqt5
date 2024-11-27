b65ec09dbda4c271d083bea868cccb65dba740f4
Huerte <huertejerald@gmail.com>
2024-11-27 17:51:57 +0800

    tagdugangan nak basic function para maka start sa PyQt5

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 #  KAYAOT SA IM NGAYAN CHARD
+import pyqt5.QtWidgets as qtw
+import sys
+
+
+class MainWindow(qtw.QMainWindow):
+  def __init__(self):
+    super().__init__()
+    self.setWindowTitle("Binoang Window")
+    self.setGeometry(100, 100, 500, 500)
+    self.show()
+
+
+def main():
+  app = qtw.QApplication(sys.argv)
+  window = MainWindow(app)
+  window.show()
+  sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+  main()
