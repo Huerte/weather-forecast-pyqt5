3cf477f642b2ce4c3321d05ee9bfa06a2f1d1f70
Huerte <huertejerald@gmail.com>
2024-11-28 08:00:13 +0800

    Tagbutangan nak nan login sanan register na window

--- register.py
+++ register.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel, QLineEdit
 
 
 class RegisterWindow:
@@ -8,6 +8,21 @@ class RegisterWindow:
     def display(self):
         window = QWidget()
         layout = QVBoxLayout()
+
+        header = QLabel("Register Account")
+        layout.addWidget(header)
+
+        name_input = QLineEdit()
+        name_input.setPlaceholderText("Enter your name")
+        layout.addWidget(name_input)
+
+        password_input = QLineEdit()
+        password_input.setPlaceholderText("Enter your password")
+        layout.addWidget(password_input)
+
+        password2_input = QLineEdit()
+        password2_input.setPlaceholderText("Verify your password")
+        layout.addWidget(password2_input)
 
         register_button = QPushButton("Register")
         register_button.clicked.connect(lambda: self.stack_widget.setCurrentIndex(2))
